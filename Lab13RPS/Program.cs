f7e8f5319f212dfefde792eb4be863026e4c0923
carterave519 <carterave519@gmail.com>
2018-05-15 17:52:19 -0400

    thrid commit

--- Lab13RPS/Program.cs
+++ Lab13RPS/Program.cs
@@ -13,18 +13,46 @@ namespace Lab13RPS
             Console.WriteLine("Welcome to Rock Paper Scissors!");
             Console.WriteLine();
             Console.WriteLine("Enter your name:");
-            Console.ReadLine();
+            ThirdPlayer Yourself = new ThirdPlayer();
+            Yourself.PlayerName = Console.ReadLine();
 
+            int input = 0;
             
             while (true)
             {
-                Console.WriteLine("Which player would you like to play as? Serena or David?");
+                Console.WriteLine("Which player would you like to play as? Serena(1) or David(2)?");
+                input = Convert.ToInt32(Console.ReadLine());
+
+             
+                if (input == 1)
+                {
+                    RockPlayer Serena = new RockPlayer();
+                    Console.WriteLine("You are playing as Serena Williams!");
+                    Console.WriteLine($"Serena picks {(Roshambo)Serena.generateRoshambo()} Your pick is {(Roshambo)Yourself.generateRoshambo()}");
+                    
+                }
+                else if (input == 2)
+                {
+                    RandomPlayer David = new RandomPlayer();
+                    Console.WriteLine("You are playing as David Beckham!");
+                    Console.WriteLine($"Serena picks {(Roshambo)David.generateRoshambo()} Your pick is {(Roshambo)Yourself.generateRoshambo()}");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry. That was not invalid input. Please enster 1, 2 or 3.");
+                    continue;
+                }
+
+                
             }
+
 
 
 
         
 
         }
+
+
     }
 }
--- Lab13RPS/Validator.cs
+++ Lab13RPS/Validator.cs
@@ -8,6 +8,22 @@ namespace Lab13RPS
 {
     class Validator
     {
+        private object Serena;
 
+        public Validator()
+        {
+
+            if (Serena == David)
+            {
+                Console.WriteLine("Tie game.");     
+            }
+            else if(Serena = 0 & David = 1)
+            {
+                Console.WriteLine("David wins!");
+            }
+
+            
     }
-}
+
+        public object David { get; }
+    }
